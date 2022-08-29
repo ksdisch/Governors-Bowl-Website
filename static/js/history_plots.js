aca4daee4279a27d900b43a56f3703652dd65032
ksdisch <102986862+ksdisch@users.noreply.github.com>
2022-08-28 19:54:18 -0500

    Add files via upload

---
+++ static/js/history_plots.js
@@ -0,0 +1,39 @@
+function buildCharts() {
+    var selector = d3.select("#wins");
+    d3.json("league_history.json").then((data) => {
+        console.log(data);
+        // get array of all the governors, convert to title case w/ function
+        // found on stackoverflow (x-axis)
+        var governorsArray = Object.keys(data).map(function toTitleCase(str) {
+            return str.replace(
+              /\w\S*/g,
+              function(txt) {
+                return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+              }
+            );
+          });
+        console.log(governorsArray);
+        // get array of all time win totals
+        // var governorWins = Object.entries(data);
+        var governorWins = Object.values(data).map(val => val.total_wins);
+        console.log(governorWins);
+        
+        // create trace for the wins bar chart
+        var winsBarData = [{
+            type: "bar",
+            x: governorsArray,
+            y: governorWins
+        }];
+
+        var winsBarLayout = {
+            title: "Total Regular Season Wins"
+        };
+
+        
+        plotly.newPlot("wins", winsBarData, winsBarLayout);
+
+    });
+}
+
+
+
---
+++ static/js/plots.js
@@ -0,0 +1,14 @@
+// function init() {
+//     var selector = d3.select("#selDataset");
+
+//     d3.json("../../team_stats.json").then((data) => {
+//         console.log(data);
+//         Object.entries(data).forEach(([key, value]) => {
+//             console.log(key)
+//             console.log(value)
+//         });
+//     });
+// }
+
+
+// init();
